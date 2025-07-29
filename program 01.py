1c9f9e43020b4bc6f4aa60e9ab1bf39a68d0932d
nimson-s <nimson0628@gmail.com>
2025-07-29 11:05:20 +0530

    program 01

---
+++ program 01.py
@@ -0,0 +1,16 @@
+print("ARITHEMATIC OPERATIONS")
+print("----------------------")
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Result:")
+c=a+b
+print("Add:",c)
+c=a-b
+print("sub:",c)
+c=a*b
+print("mul:",c)
+c=a/b
+print("div:",c)
+c=a%b
+print("mud:",c)
+      
