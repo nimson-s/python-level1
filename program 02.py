313be4751a5cfd97881f3334b132064ccf1815c9
nimson-s <nimson0628@gmail.com>
2025-07-30 15:46:35 +0530

    nimson02

---
+++ program 02.py
@@ -0,0 +1,14 @@
+print("TAKSHASHILA UNIVERSIITY")
+print("ongur,Tindivanam,Villupuram,DIST.TN,")
+print("-------------------------------------")
+print("STUDENT MARK LIST")
+print("-----------------")
+Str1=int(input("Enter the Register no:"))
+Str2=int(input("Enter the name:"))
+m1=int(input("python mark:"))
+m2=int(input("DBMS mark:"))
+m3=int(input("Accounting mark:"))
+Total=m1+m2+m3
+print("Total mark:",Total)
+Average=Total/3
+print("Average mark:",Average)
